a5e57167c4e6c3385fdb7545a0ee7a0e3c0a6f5c
Megan Horsfield <meganthadev@gmail.com>
2020-08-04 20:46:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function findMatching(drivers, str) {
 
 function fuzzyMatch(drivers, str) { 
   return driver.filter( driver => {
-    let matchLetters = driver.split('').slice(0)
+    let matchLetters = driver.split('').slice(0, string.length)
   })
 }
