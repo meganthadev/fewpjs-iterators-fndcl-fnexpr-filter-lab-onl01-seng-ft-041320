143575c3c96ad9b7670f594efff0faa53e88b224
Megan Horsfield <meganthadev@gmail.com>
2020-08-04 20:47:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function findMatching(drivers, str) {
 function fuzzyMatch(drivers, str) { 
   return driver.filter( driver => {
     let matchLetters = driver.split(' ').slice(0, string.length)
-    matchLetters = matchLetters.join('')
+    matchLetters = matchLetters.join(' ')
      return matchLetters.toLowerCase() === string.toLowerCase()
   });
 }
