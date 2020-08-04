3b7c5e46b4acef730929bee557450a6457196dbb
Megan Horsfield <meganthadev@gmail.com>
2020-08-04 20:59:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(drivers, str) {
+  let matching = drivers.filter(driver => {
+    return driver.toLowerCase() === str.toLowerCase()
+  })
+  return matching
+}
+
+function fuzzyMatch(drivers, str) { 
+  return drivers.filter( drivers => {
+    let matchedLetters = drivers.split('').slice(0, str.length)
+    matchedLetters = matchedLetters.join('')
+     return matchedLetters.toLowerCase() === str.toLowerCase()
+  });
+}
+
+function matchName(ArrayOfObj, str) {
+  return ArrayOfObj.filter( obj => {
+    return obj.name === str
+  })
+}
